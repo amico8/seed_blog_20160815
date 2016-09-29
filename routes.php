192bb964b42208f4580fe403fee4240095437985
amico <amico8amico@gmail.com>
2016-09-29 13:30:32 +0800

    routes.phpの作成

--- routes.php
+++ routes.php
@@ -1,3 +1,18 @@
 <?php
-  echo 'routes.phpを通りました';
+  echo 'routes.phpを通りました<br>';
+
+  // １．GETパラメータを取得
+  // explode関数：第二引数の文字列を、第一引数の文字列で分解し、配列で返す関数。
+  $params = explode('/', $_GET['url']);
+
+  // ２．パラメータの分解
+  $resouce = $params[0];
+  $action = $params[1];
+  $id = 0;
+  if (isset($params[2])) {
+    $id = $params[2];
+  }
+
+  // ３．コントローラの呼び出し
+  require('controllers/'. $resouce .'_controller.php');
 ?>
